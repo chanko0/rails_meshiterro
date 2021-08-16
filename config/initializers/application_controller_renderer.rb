cc470efe6dd59dc507b18476f03d6a24ceb851d1
chanko0 <ryo-suke1107@ezweb.ne.jp>
2021-08-16 03:52:13 +0000

    [Add]3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'f0a2d1b10c95ebc9f6c5ab52f420d0cb9022d67c71651e2a1bb238eb49a811b88ba876bfd3ef3196e0979cb819179e26ee2273a5fc863e362a236cb8ee685edd'
